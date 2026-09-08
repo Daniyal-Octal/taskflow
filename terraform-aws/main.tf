c5219e7718e1551252fd1ac92af58ec0a9935df6
Daniyal-Octal <daniyalkhan10904@gmail.com>
2026-09-08 19:55:20 +0000

    Add real AWS terraform config, verified apply/destroy lifecycle

---
+++ terraform-aws/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "taskflow_bucket_real" {
+  bucket = "taskflow-app-bucket-daniyal-2026"
+}
